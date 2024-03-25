aa8233bae6374364f0241df7c11c810d7345384b
zhengqi <709030194@qq.com>
2024-03-25 16:39:41 +0800

    914

---
+++ src/bin/914.rs
@@ -0,0 +1,25 @@
+use std::collections::{BTreeSet, HashMap, HashSet};
+
+fn gcd(mut x: i32, mut y: i32) -> i32 {
+    if x == 0 {
+        y
+    } else {
+        gcd(y % x, x)
+    }
+}
+pub fn has_groups_size_x(deck: Vec<i32>) -> bool {
+    let set = deck
+        .into_iter()
+        .fold(HashMap::new(), |mut map, val| {
+            map.entry(val).and_modify(|v| *v += 1).or_insert(1);
+            map
+        })
+        .into_iter()
+        .map(|(_, v)| v)
+        .collect::<BTreeSet<i32>>();
+
+    set.into_iter().fold(0, |acc, v| gcd(acc, v)) >= 2
+}
+fn main() {
+    println!("{}", has_groups_size_x(vec![1, 1, 1, 2, 2, 2, 2, 2, 2, 1]))
+}
